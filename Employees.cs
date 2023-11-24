1a6bc18b568a2bca6e8fbce373452fdd826baae3
nurudeen8 <nurudeen.durowade@optisoft.ng>
2023-11-24 09:46:57 +0100

    Initial commit

---
+++ Employees.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+
+    public interface ISalaryCalculator
+    {
+        double CalculateSalary();
+    }
+
+    //Enum to select employee roles
+
+    public enum EmployeeRole
+    {
+        Manager, 
+        Developer,
+        Designer
+    }
+
+    public delegate void EmployeeEventHandler(string message);
+
+
+    public class Employee:ISalaryCalculator
+    {
+        //Fields
+        private string name;
+        private int age;
+        private double salary;
+        private int employeeid;
+
+
+        //Event
+
+        //public event EmployeeEventHandler OnPromotion;
+
+        //Properties
+        public string Name { get { return name; } set { name = value; } }
+        public int Age { get { return age; } set { age = value; } }
+
+        public double Salary { get { return salary; } set { salary = value; } }
+
+        public int EmployeeID { get { return employeeid; } set { employeeid = value; } }
+
+        //Constructors
+
+        public Employee(string name, int age, double salary, int employeeid)
+        {
+            this.name = name;
+            this.age = age;
+            this.salary = salary;
+            this.employeeid= employeeid;
+        }
+
+        //parameterless Constructors
+        public Employee() { }
+
+
+        
+
+
+
+        ~Employee()
+        {
+
+            //Destructors in C# are methods inside the class
+            //used to destroy instances of that class when they are no longer needed
+
+            Console.WriteLine($"Destructor: Employee {name} destroyed");
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {name}, Age: {age}, Salary: {salary}");
+        }
+
+        // Method to calculate salary (from ISalaryCalculator interface)
+        public double CalculateSalary()
+        {
+            // Sample salary calculation logic
+            return salary * 12; // Assuming monthly salary, calculate annual salary
+        }
+
+        public void Promote(EmployeeRole newRole)
+        {
+            Console.WriteLine($"Employee {name} promoted to {newRole}");
+
+            //OnPromotion?.Invoke($"Employee {name} promoted to {newRole}");
+        }
+
+        // Method to promote employee and invoke event
+       
+
+
+    }
+
+   
+
+   
+
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace EmployeeManagement.Employees;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        Employee em1 = new Employee("Bamidele Fathia", 25, 150000.55, 101);
+        Employee em2 = new Employee("Opeyemi Coker", 26, 155000.65, 102);
+        Employee em3 = new Employee("Tolu Bashir", 35, 180000.75, 103);
+        Employee em4 = new Employee("Adeola Seun", 47, 200000.15, 104);
+        Employee em5 = new Employee("Simi Adele", 54, 120000.35, 105);
+        Employee em6 = new Employee("Bola Segun", 34, 250000.45, 106);
+        Employee em7 = new Employee("Hamid Jamiu", 27, 130000.15, 107);
+        Employee em8 = new Employee("Olakunle Joseph", 28, 160000.05, 108);
+        Employee em9 = new Employee("Peter Adetola", 33, 140000.55, 109);
+        Employee em10 = new Employee("Collins ikechukwu", 32, 333000.55, 110);
+
+        //Display employee Information
+        Console.WriteLine("Initial Employee Information:");
+        em1.DisplayInfo();
+        Console.WriteLine("Initial Employee Information:");
+        em2.DisplayInfo();
+        Console.WriteLine("Initial Employee Information:");
+        em3.DisplayInfo();
+
+        //Update Employee Information
+
+        em1.Name = "Jane Doe";
+        em1.EmployeeID = 113;
+        em1.Salary = 120000.33;
+
+        // Display updated information
+        Console.WriteLine("\nUpdated Employee Information:");
+        em1.DisplayInfo();
+
+        // Calculate and display annual salary using interface method
+        double annualSalary = em1.CalculateSalary();
+        Console.WriteLine($"\nAnnual Salary: {annualSalary}");
+
+     
+        
+        em1.Promote(EmployeeRole.Manager);
+
+       
+        Console.WriteLine("\nEnd of program");
+
+
+       
+
+    }
+
+
+
+
+}
